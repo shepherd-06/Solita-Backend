a3d93f33ba5373e2143c48d0d83a6881b1a1446b
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-02-06 11:27:43 +0200

    debug turned to false for server

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "172.104.158.15",
