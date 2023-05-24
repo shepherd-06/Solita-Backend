ac8541327ad47e13798fa529d12366cfd599f337
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-05-25 02:46:55 +0300

    Add new station API updated

--- ops/api/add_station.py
+++ ops/api/add_station.py
@@ -1,10 +1,15 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from ops.serializer import StationSerializer
+from random import randint, uniform
 
 
 # @method_decorator(csrf_exempt, name='dispatch')
 class AddStation(APIView):
+
+    REQUIRED_PARAMS = ["coordinate_x", "coordinate_y"]
+    # at least one of them has to be present
+    OPTIONAL_PARAMS = ["name_en", "name_fi", "name_sw"]
 
     def post(self, request):
         """_summary_
@@ -26,11 +31,34 @@ class AddStation(APIView):
                 "options": names,
             }, status=400)
 
+        for item in self.REQUIRED_PARAMS:
+            if item not in data:
+                return JsonResponse({
+                    "message": "Coordinates are required",
+                }, status=400)
+
+        data["station_id"] = randint(10000000, 500000000)
+
         serializer = StationSerializer(data=data)
         if serializer.is_valid():
-            obj = serializer.save()
+            station_obj = serializer.save()
+            station_data = station_obj.__dict__
+            station_data.pop('_state')
+            station_data.pop('id')
+            station_data["created_at"] = station_data["created_at"].timestamp()
+            station_data["updated_at"] = station_data["updated_at"].timestamp()
+            data = {
+                "start_from": 0,
+                "return_to": 0,
+                "avg_departure_distance": 0,
+                "avg_return_distance": 0,
+                "popular_return": 0,
+                "popular_departure": 0,
+                "station": station_data,
+            }
             return JsonResponse({
-                "message": "{} station created".format(obj)
+                "message": "{} station created".format(station_obj),
+                "data": data
             }, status=201)
         else:
             return JsonResponse({
